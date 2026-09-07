ac27db88f4ddc1b2b6992ae869c902ffa691eaff
Senn <sennpichayawadee@gmail.com>
2026-09-08 02:48:41 +0700

    Set up header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css'   //Load the css file called Header.css that is in the same folder as the one i am right now (header.tsx)
+
+function Header() {
+    return (
+        <header className = "header">
+            <h1>KnowYourWings</h1>
+        </header>
+    )
+}
+
+export default Header
